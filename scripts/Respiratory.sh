17900cb2371f8394b6ab79b2e14ffb53399af9cd
LamNicole <nicole.lam@yale.edu>
2022-08-10 18:51:37 -0700

    various updates

--- scripts/Respiratory.sh
+++ scripts/Respiratory.sh
@@ -25,5 +25,14 @@ python -u main_informer.py --model informer --data Respiratory --root_path ./dat
 # Test 9: MSE: 0.054628804326057434 (Patient 4, 2000 datapoints, 0.1 seconds forward, epochs 6)
 python -u main_informer.py --model informer --data Respiratory --root_path ./data/ --freq s --data_path Respiratory2000/4.csv --seq_len 300 --c_out 1 --enc_in 1 --pred_len 2 --dec_in 1 --target Amplitude --features S --itr 1 --train_epochs 6 --patience 30
 
-# Test 10: 0.061359722167253494 (Patient 4, 2000 datapoints, 0.2 seconds forward, epochs 6)
+# Test 10: MSE: 0.061359722167253494 (Patient 4, 2000 datapoints, 0.2 seconds forward, epochs 6)
 python -u main_informer.py --model informer --data Respiratory --root_path ./data/ --freq s --data_path Respiratory2000/4.csv --seq_len 300 --c_out 1 --enc_in 1 --pred_len 4 --dec_in 1 --target Amplitude --features S --itr 1 --train_epochs 6 --patience 30
+
+# Test 11: MSE: 0.008109519258141518 (Patient 4, 5000 datapoints, 0.05 seconds forward, epochs 6)
+python -u main_informer.py --model informer --data Respiratory --root_path ./data/ --freq s --data_path Respiratory5000/4.csv --seq_len 300 --c_out 1 --enc_in 1 --pred_len 1 --dec_in 1 --target Amplitude --features S --itr 1 --train_epochs 6 --patience 30
+
+# Test 12: MSE: 0.011885087937116623 (Patient 4, 5000 datapoints, 0.1 seconds forward, epochs 6)
+python -u main_informer.py --model informer --data Respiratory --root_path ./data/ --freq s --data_path Respiratory5000/4.csv --seq_len 300 --c_out 1 --enc_in 1 --pred_len 2 --dec_in 1 --target Amplitude --features S --itr 1 --train_epochs 6 --patience 30
+
+# Test 13: MSE:0.013456298969686031 (Patient 4, 5000 datapoints, 0.2 seconds forward, epochs 6)
+python -u main_informer.py --model informer --data Respiratory --root_path ./data/ --freq s --data_path Respiratory5000/4.csv --seq_len 300 --c_out 1 --enc_in 1 --pred_len 4 --dec_in 1 --target Amplitude --features S --itr 1 --train_epochs 6 --patience 30
